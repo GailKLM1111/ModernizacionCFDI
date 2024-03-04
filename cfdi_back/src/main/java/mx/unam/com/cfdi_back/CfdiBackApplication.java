537528ca6c21df2354dfeb3476bd936a95a18635
Tania Rubí <taniarubi@ciencias.unam.mx>
2024-03-04 17:42:41 -0600

    Se agregan los botones de prueba

--- cfdi_back/src/main/java/mx/unam/com/cfdi_back/CfdiBackApplication.java
+++ cfdi_back/src/main/java/mx/unam/com/cfdi_back/CfdiBackApplication.java
@@ -2,6 +2,8 @@ package mx.unam.com.cfdi_back;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @SpringBootApplication
 public class CfdiBackApplication {
@@ -9,5 +11,12 @@ public class CfdiBackApplication {
     public static void main(String[] args) {
         SpringApplication.run(CfdiBackApplication.class, args);
     }
+
+    @GetMapping("/hola")
+    public String DecirHola(@RequestParam(value = "miNombre", defaultValue = "Mundo") String nombre) {
+        System.out.println("Hola");
+        return String.format("Hola %s!", nombre);
+    }
+
 
 }
--- cfdi_front/src/App.js
+++ cfdi_front/src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import { Button} from 'react-bootstrap';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="App">
+        <header className="App-header">
+            <Button
+                onClick={() => {
+                    hola()
+                }}
+            >
+                Click click
+            </Button>
+
+            <Button variant="contained">Contained</Button>
+
+        </header>
+      </div>
   );
+}
+
+function hola(){
+
+    var xhr = new XMLHttpRequest();
+
+    var miNombre = "Gail";
+
+    xhr.responseType = 'text';
+
+    xhr.open("GET", "http://localhost:8080/hola?" + "miNombre=" + miNombre, true );
+
+    xhr.onload = function () {
+        var json = JSON.parse(xhr.response);
+        console.log(json.url);
+        // addAlert("Reporte generado correctamente", json.url)
+    }
+
+    xhr.send(null);
+
 }
 
 export default App;
